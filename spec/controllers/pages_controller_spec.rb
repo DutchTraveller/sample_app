4e48b98085814ad24504b84ebd1fc1a0c2dbc9fa
Joop Verbaken <JoopVerbaken@GMail.com>
2010-05-25 11:16:06 -0700

    Done with Static Pages Joop

---
+++ spec/controllers/pages_controller_spec.rb
@@ -0,0 +1,66 @@
+require 'spec_helper'
+
+describe PagesController do
+  integrate_views
+
+  #Delete these examples and add some real ones
+  it "should use PagesController" do
+    controller.should be_an_instance_of(PagesController)
+  end
+
+
+  describe "GET 'home'" do
+    class PagesController < ApplicationController
+    def home
+      @title = "Home"
+    end
+
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_tag("title",
+      "Ruby on Rails Tutorial Sample App | Home")
+    end
+
+  end
+ 
+  describe "GET 'contact'" do
+    def contact
+      @title = "Contact"
+    end
+
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_tag("title",
+      "Ruby on Rails Tutorial Sample App | Contact")
+    end
+
+  end
+
+  describe "GET 'about'" do
+    def about
+      @title = "About"
+    end
+
+      it "should be successful" do
+        get 'about'
+        response.should be_success
+      end
+
+      it "should have the right title" do
+        get 'about'
+        response.should have_tag("title",
+        "Ruby on Rails Tutorial Sample App | About")
+      end
+
+    end
+end
